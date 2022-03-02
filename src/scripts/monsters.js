32da4cf707a1dbe6dd573488526dd1f230006129
H0rn0chse <h0rn0chse@gmx.de>
2022-03-02 23:11:45 +0100

    Addded first assets

---
+++ src/scripts/monsters.js
@@ -0,0 +1,13 @@
+
+const dataPromise = fetch("../assets/monsters.json")
+    .then(response => response.json());
+
+export async function getMonsterDetails (monster) {
+    const data = await dataPromise;
+    return data[monster];
+}
+
+export async function getAllMonsterIds () {
+    const data = await dataPromise;
+    return Object.keys(data);
+}
